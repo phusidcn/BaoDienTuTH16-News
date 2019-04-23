40fad8c80f0ca3c1f4b0b92309403638f6f6c468
lequocduyquang <duyquangbtx@gmail.com>
2019-04-23 15:32:15 +0700

    Connect success mongo atlas

--- server/server.js
+++ server/server.js
@@ -1,16 +1,59 @@
-const express = require('express')
-const morgan = require('morgan')
+// Server.js
 
-const app = express()
+// Set up =================================================== 
 
-app.use(morgan())
+const express       = require('express')
+const exphbs        = require('express-handlebars')
+const mongoose      = require('mongoose')
+const passport      = require('passport')
+const flash         = require('connect-flash')
 
-const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello Node')
+const morgan        = require('morgan')
+const cookieParser  = require('cookie-parser')
+const bodyParser    = require('body-parser')
+const session       = require('express-session')
+
+const app   = express()
+const PORT  = process.env.PORT || 3000
+
+const configDB = require('./config/database')
+
+// Configuration =============================================
+mongoose.connect(configDB.url, {
+    useNewUrlParser: true
+}) 
+.then(() => {
+    console.log('Connect to MongoDB Atlas')
+})
+.catch(err => {
+    console.log(err)
+    throw err
 })
 
+// require('./config/passport')(passport) // pass passport for configuration
+
+
+// Set up Express application
+app.use(morgan('dev')) // log every requests to the console
+app.use(cookieParser()) // read cookies (need for auth)
+app.use(bodyParser())
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))   
+app.set('view engine', 'handlebars')    // set up handlebars for templating
+
+// required for passport
+app.use(session({
+    secret: 'baodientunews'
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash()) // flash messages
+
+// routes ====================================================
+// require('./app/routes')(app, passport)
+
+// Running server
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`)
 })
