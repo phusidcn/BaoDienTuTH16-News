fdb906e0396beed36eb8eefaaca60dc693fb9892
Huynh Lam Phu Si <phusidcn@hotmail.com.vn>
2019-04-29 00:37:45 +0700

    Fix Modal function
    On context click open modal fix

--- assets/js/context.js
+++ assets/js/context.js
@@ -19,14 +19,15 @@ window.onload = function () {
 //Get Item 
 var item;
 // Get the modal
-var modalInfo = document.getElementById('infoModal');
-var modalChange = document.getElementById('changeModal');
+//var modalInfo = document.getElementById('infoModal');
+//var modalChange = document.getElementById('changeModal');
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal Info
 function showModalInfo() {
+	var modalInfo = document.getElementById('infoModal');
 	//var list = item.currentTarget.children;
 	//document.getElementById("Name").innerHTML = 'Họ tên: ' + list[1].innerHTML + ' ' + list[2].innerHTML;
 	//document.getElementById("Category").innerHTML = 'Chuyên mục:' + list[3].innerHTML;
@@ -35,6 +36,7 @@ function showModalInfo() {
 
 // When the user clicks on the button, open the modal Change
 function showModalChange(){
+	var modalChange = document.getElementById('changeModal');
 	var list = item.currentTarget.children;
 	document.getElementById('categoryChange').innerHTML = list[3].innerHTML;
 	modalChange.style.display = "block";
@@ -42,6 +44,8 @@ function showModalChange(){
 
 // When the user clicks on <span> (x), close the modal
 function closeModal() {
+	var modalInfo = document.getElementById('infoModal');
+	var modalChange = document.getElementById('changeModal');
 	modalInfo.style.display = "none";
 	modalChange.style.display = "none";
 }
