c0efa99c77800a76acf294297605f6b480d38ce7
lequocduyquang <duyquangbtx@gmail.com>
2019-05-13 21:31:01 +0700

    Change hbs guest

--- api/app.js
+++ api/app.js
@@ -1,29 +1,33 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const path = require('path')
-const cors = require('cors')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
 
 const errorHandler = require('./middleware/errorHandler')
-const postRoutes = require('./routes/post')
 const app = express()
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
 const PORT = 3000 || process.env.PORT
 
-app.use(cors())
-
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/rest-api', {
-    useNewUrlParser: true
-})
-
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname + 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/api/post', postRoutes)
 app.use(errorHandler)
 
+app.get('/', (req, res) => {
+    res.render('guest/guestHome')
+})
+
+app.get('/category', (req, res) => {
+    res.render('guest/guestCategory')
+})
+
+app.get('/post', (req, res) => {
+    res.render('guest/guestPost')
+})
 
 app.listen(3000, () => {
     console.log(`App running on PORT ${PORT}`)
