7cb11013d6e288b1b641bbcd16b3e75f84e5793c
lequocduyquang <duyquangbtx@gmail.com>
2019-05-16 11:07:41 +0700

    layput admin

--- api/controllers/adminController.js
+++ api/controllers/adminController.js
@@ -10,3 +10,35 @@ exports.index = async (req, res) => {
         console.log(err)
     }
 } 
+
+exports.editor = async (req, res) => {
+    try {
+        await res.render('admin/editor')
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+exports.user = async (req, res) => {
+    try {
+        await res.render('admin/premium-user')
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+exports.profile = async (req, res) => {
+    try {
+        await res.render('admin/profile')
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+exports.writer = async (req, res) => {
+    try {
+        await res.render('admin/writer')
+    } catch (err) {
+        console.log(err)
+    }
+}
--- api/routes/admin/index.js
+++ api/routes/admin/index.js
@@ -4,5 +4,9 @@ const router = express.Router()
 
 router.all('/*', adminController.all)
 router.get('/', adminController.index)
+router.get('/editor', adminController.editor)
+router.get('/premium-user', adminController.user)
+router.get('/profile', adminController.profile)
+router.get('/writer', adminController.writer)
 
 module.exports = router
