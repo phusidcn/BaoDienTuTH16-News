459971a008bdc5392ab818f4e36855f06d0e2b07
Le Quoc Thai <1612608@student.hcmus.edu.vn>
2019-05-24 02:39:23 +0700

    add some objects in data.js

--- api/data.js
+++ api/data.js
@@ -33,7 +33,13 @@ const subscriber1Id = mongoose.Types.ObjectId()
 const subscriber2Id = mongoose.Types.ObjectId()
 const subscriber3Id = mongoose.Types.ObjectId()
 
+const editor1Id = mongoose.Types.ObjectId()
+const editor2Id = mongoose.Types.ObjectId()
+const editor3Id = mongoose.Types.ObjectId()
 
+const admin1Id = mongoose.Types.ObjectId()
+const admin2Id = mongoose.Types.ObjectId()
+const admin3Id = mongoose.Types.ObjectId()
 
 
 module.exports = {
@@ -180,5 +186,169 @@ module.exports = {
     ],
 
 
-    // Thái làm tiếp Writer, Editor, Guest, Sucscriber, với admin.
+    // Thái làm tiếp Writer, Editor, Guest, subscriber, với admin.
+
+    //writer
+    "writer":[
+        {
+            "_id": writer1Id,
+            "email": "lequocthai1998@gmail.com",
+            "password": "gagtrxrz111",
+            "userName": "Lê Quốc Thái",
+            "birthday": "26/05/1998",
+            "address": "333 Lý Thái Tổ, P9, Q10",
+            "company": "HCMUS",
+            "image": "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2452&q=80"
+        },
+        {
+            "_id": writer2Id,
+            "email": "lequocduyquang@gmail.com",
+            "password": "123456789",
+            "userName": "Lê Quốc Duy Quang",
+            "birthday": "01/01/1998",
+            "address": "TPHCM",
+            "company": "UIT",
+            "image": "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2452&q=80"
+        },
+        {
+            "_id": writer3Id,
+            "email": "lequocthai@gmail.com",
+            "password": "gagtrxrz111",
+            "userName": "Huỳnh Lâm Phú Sĩ",
+            "birthday": "01/01/1998",
+            "address": "333 Lý Thái Tổ, P9, Q10",
+            "company": "BK",
+            "image": "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2452&q=80"
+        }
+    ],
+
+    //editor
+    "editor":[
+        {
+            "_id": editor1Id,
+            "email": "lequocthai1998@gmail.com",
+            "password": "gagtrxrz111",
+            "userName": "Lê Quốc Sang",
+            "birthday": "26/05/1998",
+            "address": "333 Lý Thái Tổ, P9, Q10",
+            "company": "KMS",
+            "image": "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2452&q=80"
+        },
+        {
+            "_id": editor2Id,
+            "email": "lequocduyquang@gmail.com",
+            "password": "123456789",
+            "userName": "Lê Quốc",
+            "birthday": "01/01/1998",
+            "address": "TPHCM",
+            "company": "HCMUS",
+            "image": "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2452&q=80"
+        },
+        {
+            "_id": editor3Id,
+            "email": "lequoc@gmail.com",
+            "password": "gagtrxrz111",
+            "userName": "Huỳnh Lâm Phú Sỹ",
+            "birthday": "01/01/1998",
+            "address": "333 Lý Thái Tổ, P9, Q10",
+            "company": "VNG",
+            "image": "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2452&q=80"
+        }
+    ],
+
+    //guest
+    "guest":[
+        {
+            "_id": guest1Id,
+            "email": "lequocthai1998@gmail.com",
+            "password": "gagtrxrz111",
+            "userName": "Lê Văn Thái",
+            "birthday": "26/05/1998",
+            "address": "333 Lý Thái Tổ, P9, Q10",
+            "image": "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2452&q=80"
+        },
+        {
+            "_id": guest2Id,
+            "email": "lequocduyquang@gmail.com",
+            "password": "123456789",
+            "userName": "Lê Duy Quang",
+            "birthday": "01/01/1998",
+            "address": "TPHCM",
+            "image": "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2452&q=80"
+        },
+        {
+            "_id": guest3Id,
+            "email": "lequocthai1998@gmail.com",
+            "password": "gagtrxrz111",
+            "userName": "Huỳnh Phú Sĩ",
+            "birthday": "01/01/1998",
+            "address": "333 Lý Thái Tổ, P9, Q10",
+            "image": "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2452&q=80"
+        }
+    ],
+
+    //subscriber
+    "subscriber":[
+        {
+            "_id": subscriber1Id,
+            "email": "lequocthai1998@gmail.com",
+            "password": "gagtrxrz111",
+            "userName": "Lê Quốc Quân",
+            "birthday": "26/05/1998",
+            "address": "333 Lý Thái Tổ, P9, Q10",
+            "accountDuration": "30",
+            "image": "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2452&q=80"
+        },
+        {
+            "_id": subscriber2Id,
+            "email": "lequocduyquang@gmail.com",
+            "password": "123456789",
+            "userName": "Lê Quốc Quang",
+            "birthday": "01/01/1998",
+            "address": "TPHCM",
+            "accountDuration": "3",
+            "image": "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2452&q=80"
+        },
+        {
+            "_id": subscriber3Id,
+            "email": "lequoc@gmail.com",
+            "password": "gagtrxrz111",
+            "userName": "Huỳnh Sĩ",
+            "birthday": "01/01/1998",
+            "address": "333 Lý Thái Tổ, P9, Q10",
+            "accountDuration": "10",
+            "image": "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2452&q=80"
+        }
+    ],
+
+    //admin
+    "admin":[
+        {
+            "_id": admin1Id,
+            "email": "lequocthai1998@gmail.com",
+            "password": "gagtrxrz111",
+            "userName": "Lê Quốc Tân",
+            "birthday": "26/05/1998",
+            "address": "333 Lý Thái Tổ, P9, Q10",
+            "image": "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2452&q=80"
+        },
+        {
+            "_id": admin2Id,
+            "email": "lequocduyquang@gmail.com",
+            "password": "123456789",
+            "userName": "Lê Quốc Duy Sĩ",
+            "birthday": "01/11/1998",
+            "address": "TPHCM",
+            "image": "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2452&q=80"
+        },
+        {
+            "_id": admin3Id,
+            "email": "lequocthai@gmail.com",
+            "password": "gagtrxrz111",
+            "userName": "Huỳnh Lâm Phú Quang",
+            "birthday": "01/05/1998",
+            "address": "333 Lý Thái Tổ, P9, Q10",
+            "image": "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2452&q=80"
+        }
+    ],
 }
