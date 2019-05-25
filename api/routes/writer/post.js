668541919e1999e2ea056e993d80157dfb375f90
lequocduyquang <duyquangbtx@gmail.com>
2019-05-25 19:27:59 +0700

    add new route

--- api/routes/writer/post.js
+++ api/routes/writer/post.js
@@ -38,4 +38,65 @@ router.post('/create', (req, res) => {
         });
 })
 
+router.get('/edit/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            res.render('admin/posts/edit', {
+                post: post
+            })
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+// router.put('/edit/:id', (req, res) => {
+//     Post.findOne({ _id: req.params.id })
+//         .then(post => {
+//             if (req.body.allowComments) {
+//                 allowComments = true;
+//             } else {
+//                 allowComments = false;
+//             }
+
+//             post.title = req.body.title;
+//             post.status = req.body.status;
+//             if (!req.body.allowComments) {
+//                 post.allowComments = false;
+//             } else {
+//                 post.allowComments = true;
+//             }
+//             post.description = req.body.description;
+//             if (!isEmpty(req.files)) {
+//                 let file = req.files.file;
+//                 filename = Date.now() + '-' + file.name;
+//                 post.file = filename;
+
+//                 file.mv('./public/uploads/' + filename, (err) => {
+//                     if (err) {
+//                         throw err;
+//                     }
+//                 });
+                
+//             }
+
+//             post.save()
+//                 .then(updatedPost => {
+//                     req.flash('success_message', `Post ${updatedPost} was successfully updated`);
+//                     res.redirect('/admin/posts');
+//                 })
+//                 .catch(err => {
+//                     console.log(err);
+//                 });
+//         });
+// });
+
+// router.delete('/:id', (req, res) => {
+//     Post.deleteOne({ _id: req.params.id })
+//         .then(() => {
+//             req.flash('success_message', 'Post was successfully deleted');
+//             res.redirect('/admin/posts');
+//         });
+// });
+
 module.exports = router
