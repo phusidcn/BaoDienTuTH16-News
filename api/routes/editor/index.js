75112c6f2c91d6326b4f885f0cb072ebaf99064d
Huynh Lam Phu Si <phusidcn@hotmail.com.vn>
2019-05-29 23:08:16 +0700

    Add Editor Authentication
    Login and register for editor

--- api/routes/editor/index.js
+++ api/routes/editor/index.js
@@ -8,7 +8,10 @@ router.get('/view-censored', editorController.viewCensored)
 router.get('/view-denied', editorController.viewDenied)
 router.get('/view-draft', editorController.viewDraft)
 router.get('/censor-content', editorController.censorContent)
-// router.get('/view-list-content', editorController.viewListContent)
+router.get('/register',editorController.register)
+router.post('/register',editorController.registerSubmit)
+router.get('/login',editorController.login)
+router.post('/login',editorController.loginPost)
 
 
 module.exports = router
