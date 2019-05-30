9d3eef765f013679274fef3ad45ad8c9baa52331
lequocduyquang <duyquangbtx@gmail.com>
2019-05-30 21:04:26 +0700

    facebook authen

--- api/config/dev.js
+++ api/config/dev.js
@@ -3,7 +3,7 @@ module.exports = {
     mongoURI: 'mongodb://localhost/minimum',
     googleClientID: '672674025045-ahrpo0151l2kkvrrscb5btdmrhmeihia.apps.googleusercontent.com',
     googleClientSecret: '96NMIjaiZA3SuBDDJw3qT2RZ',
-    facebookClientID: '',
-    facebookClientSecret: '',
+    facebookClientID: ' 624263214753592',
+    facebookClientSecret: '●●●●●●●●',
     port: 3000
 }
