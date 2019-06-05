ca0be9816f2c71c71c8306b1e7ab97066f875449
Huynh Lam Phu Si <phusidcn@hotmail.com.vn>
2019-06-05 13:43:11 +0700

    Add authentication for subscriber
    Include views controller and route

---
+++ api/middleware/subscriberPassport.js
@@ -0,0 +1,44 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+
+const Subscriber = require('../models/Subscriber')
+
+module.exports = function(passport) {
+    passport.use(
+        'subscriberLocal',
+        new LocalStrategy({
+            usernameField: 'email'
+        }, (email, password, done) => {
+            Subscriber.findOne({
+                email: email
+            }).then(subscriber => {
+                if(subscriber) {
+                    return done(null, false, {
+                        message: 'This email is already taken by other user'
+                    })
+                }
+            })
+
+            bcrypt.compare(password, subscriber.password, (err, isMatch) => {
+                if (err) throw err;
+                if (isMatch) {
+                    return done(null. subscriber);
+                } else {
+                    return done(null, false, {
+                        message: 'Password not match'
+                    })
+                }
+            })
+        })
+    )
+
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    })
+
+    passport.deserializeUser(function (id, done) {
+        Subscriber.findById(id, function(err, user) {
+            done(err, user)
+        })
+    })
+}
